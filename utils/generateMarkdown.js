a4c2d48de9ae3fa24aa8ea023d40a5c7fffc04d6
jillo <jill.l.oreilly@gmail.com>
2024-02-02 19:23:06 +0000

    Add README markdown code to template literal in generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,58 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${setLicense(data.license)};
+
+## Description
+
+${data.description}
+
+## Table of contents
+[Installation](#Installation)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+
+## License 
+
+${data.license}
+
+## Questions
+${data.github}
+
+${data.email}
+
+
+
+
+
+
+
+
+
 `;
 }
 
 module.exports = generateMarkdown;
+
+function setLicense(license) {
+  if (license === 'None') {
+    return ''
+  } else {
+    return ``// render license badge here, replace apache with ${license}
+  }
+}
