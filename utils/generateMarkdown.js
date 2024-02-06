38d7d1458f51612e2a0e3530e93f1549d2691ba3
jillo <jill.l.oreilly@gmail.com>
2024-02-06 21:40:15 +0000

    Expand renderLicense() to add some license info and link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -23,13 +23,42 @@ function renderLicenseLink(license) {
 }
 
 // Function to render License section
+// function renderLicense(license) {
+//   if (license !== 'None') {
+//     return (
+//       `## License
+//   This project is licensed under the **${license}** license`
+//     )
+//   } else {
+//     return `` // Return empty string
+//   }
+// }
+
 function renderLicense(license) {
-  if (license !== 'None') {
+  if (license === 'Apache License 2.0') {
     return (
       `## License
-  This project is licensed under the **${license}** license`
+  This project is licensed under the **${license}** license.
+  
+  A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+  <a href='http://www.apache.org/licenses/' target='_blank'> http://www.apache.org/licenses/</a>`
     )
-  } else {
+  } else if (license === 'MIT') { 
+    return (
+      `## License
+  This project is licensed under the **${license}** license.
+      
+  A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+    )
+  } else if (license === 'GNU General Public License v3.0') {
+    return (
+      `## License
+  This project is licensed under the **${license}** license.
+      
+  Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+  <a href='https://www.gnu.org/licenses/gpl-3.0.en.html' target='_blank'>https://www.gnu.org/licenses/gpl-3.0.en.html</a>`
+    )
+  } else if (license === 'None') {
     return `` // Return empty string
   }
 }
